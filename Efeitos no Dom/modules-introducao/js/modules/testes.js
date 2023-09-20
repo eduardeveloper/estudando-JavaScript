a4dbcc1a53674e33da1b452db3352ad0ee7d5e6c
eduardeveloper <eduardeveloper90@gmail.com>
2023-09-20 11:39:07 -0300

    modules, aulas e exercicios

---
+++ Efeitos no Dom/modules-introducao/js/modules/testes.js
@@ -0,0 +1,11 @@
+export function teste() {
+  console.log('isso e um teste')
+}
+
+export function teste2() {
+  console.log('isso e um teste mesmo')
+}
+
+var nome = 'Eduardo';
+
+export const senha = 25891211
